230eb71569d56acefa2ae72d34a5fcc1d4ad2d44
Anandhuvimalan <88484102+Anandhuvimalan@users.noreply.github.com>
2024-07-29 22:53:24 +0530

    Delete eyecarehospital directory

--- eyecarehospital/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eyecarehospital.settings')
-application = get_wsgi_application()
-
-app = application  # Add this line
